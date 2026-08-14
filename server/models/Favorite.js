dac1f1e38cbbce5f5802ad6dfed4ed1dc0db998f
Dipanjan Jana <dipanjanjana001@gmail.com>
2026-08-14 11:36:36 +0530

    feat(db): create User model and link Favorite schema to userId

--- server/models/Favorite.js
+++ server/models/Favorite.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const FavoriteSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   trackId: { type: Number, required: true },
   trackName: { type: String, required: true },
   artistName: { type: String, required: true },
